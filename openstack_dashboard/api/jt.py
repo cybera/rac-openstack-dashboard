08a3724f4e51014e8bc79828e8365e464f111b04
Cybera Sysadmin <sysadmin@cybera.ca>
2016-11-23 15:46:29 -0700

    Adding support for Leased Instances

--- openstack_dashboard/api/jt.py
+++ openstack_dashboard/api/jt.py
@@ -1,7 +1,47 @@
+from dateutil import parser
+from django.conf import settings
+from horizon import conf
 from openstack_dashboard.api import base
 from openstack_dashboard.api import swift
-import requests
 import json
+import MySQLdb
+import requests
+
+def _dbconnect():
+    username = getattr(settings, 'RAC_MYSQL_USERNAME')
+    password = getattr(settings, 'RAC_MYSQL_PASSWORD')
+    host = getattr(settings, 'RAC_MYSQL_HOST')
+    return MySQLdb.connect(host=host,user=username,passwd=password,db='rac_information')
+
+def is_leased_flavor(flavor_name):
+    leased_flavors = getattr(settings, 'RAC_LEASED_FLAVORS', [])
+    return flavor_name in leased_flavors
+
+def get_instance_lease(instance_id, project_id, region):
+    try:
+        db = _dbconnect()
+        c = db.cursor()
+        query = "SELECT date_format(expiration_date, '%%M %%d, %%Y %%h:%%i %%p') from instance_leases where instance_id = %s and project_id = %s and region = %s"
+        data = (instance_id, project_id, region)
+        c.execute(query, data)
+        instance_lease = c.fetchone()
+        if instance_lease is not None:
+            return instance_lease[0]
+        return None
+    except MySQLdb.Error, e:
+        print(str(e))
+        return None
+
+def set_instance_lease(instance_id, project_id, region, lease):
+    try:
+        db = _dbconnect()
+        c = db.cursor()
+        query = "INSERT INTO instance_leases (instance_id, project_id, region, expiration_date) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE expiration_date = %s"
+        data = (instance_id, project_id, region, lease, lease)
+        c.execute(query, data)
+        db.commit()
+    except Exception as e:
+        print(str(e))
 
 def get_swift_quota(request):
     if not base.is_service_enabled(request, "object-store"):
