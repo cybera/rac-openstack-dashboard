129eda010e9b425d1ff0bcd4b1d1963f77a24c05
Cybera Sysadmin <sysadmin@cybera.ca>
2017-06-24 22:29:12 -0600

    Removing RAC Usage dashboard in favor of nectar_dashboard package

--- openstack_dashboard/dashboards/project/rac_usage/panel.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-import horizon
-
-
-class RACUsage(horizon.Panel):
-    name = _("Usage Graphs")
-    slug = 'rac_usage'
--- openstack_dashboard/enabled/_1935_rac_usage.py
+++
@@ -1,5 +0,0 @@
-PANEL = 'Usage Graphs'
-PANEL_DASHBOARD = 'project'
-PANEL_GROUP = 'rac_usage'
-
-ADD_PANEL = 'openstack_dashboard.dashboards.project.rac_usage.panel.RACUsage'
